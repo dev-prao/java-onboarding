2b1d5670ddb668e88bb34e45aec6114a1af4a9f7
prao <pch5521@gmail.com>
2023-10-16 17:22:20 +0900

    feat: [Problem4] 문자열을 순회하며 청개구리 사전 정보를 바탕으로 알파벳 변환 기능
    - 알파벳 외의 문자는 그대로 출력
    - 유니코드를 활용
     - 알파벳 대문자는 알파벳 대문자로 변환
     - 알파벳 소문자는 알파벳 소문자로 변환

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,15 +1,39 @@
 package onboarding;
 
-public class Problem4 {
-    public static String solution(String word) {
-        String answer = "";
-        return answer;
-    }
+class Problem4 {
 
-    public class Validation {
+	private Problem4() {}
 
-        public boolean length(String word) {
-            return 1 <= word.length() && word.length() <= 1000;
-        }
-    }
+	static Validation validation = new Validation();
+	static Converter converter = new Converter();
+
+	public static String solution(String word) {
+		if (validation.length(word)) {
+			return converter.convert(word);
+		}
+		return "";
+	}
+
+	static class Validation {
+
+		public boolean length(String word) {
+			return !word.isEmpty() && word.length() <= 1000;
+		}
+	}
+
+	static class Converter {
+
+		public String convert(String word) {
+			char[] words = word.toCharArray();
+			for (int i = 0; i < words.length; i++) {
+				if (65 <= words[i] && words[i] <= 90) {
+					words[i] = (char)(155 - words[i]);
+				}
+				if (97 <= words[i] && words[i] <= 122) {
+					words[i] = (char)(219 - words[i]);
+				}
+			}
+			return new String(words);
+		}
+	}
 }
