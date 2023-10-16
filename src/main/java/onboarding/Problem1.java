b8145356af390b2e25ae69d77ecc90bda9c6935c
prao <pch5521@gmail.com>
2023-10-16 12:19:31 +0900

    feat: [Problem1]입력값 유효성 검사 기능
    - validate: 입력값 유효성 검사 실행
    - isValidPage: 페이지 번호 1 이상 400 이하 확인
    - isOdd: 페이지가 홀수인지 확인
    - isEven: 페이지가 짝수인지 확인
    - containsPageNumber: 모든 페이지 번호 기재 확인
    - isContinuous: 페이지 번호가 연속적인지 확인
    - isNotFirstPageOrLastPage: 시작 면이나 마지막 면이 나오지 않는지 확인
    - isSizeTwo: 입력받은 페이지 리스트의 길이가 2인지 확인

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -8,3 +8,45 @@ class Problem1 {
         return answer;
     }
 }
+
+class Validation {
+    private static final int FIRST_PAGE = 1;
+    private static final int LAST_PAGE = 400;
+
+    public boolean validate(List<Integer> pages) {
+        return isValidPage(pages) && isOdd(pages.get(0)) && isEven(pages.get(1)) && cotainsPageNumber(pages)
+            && isContinuous(pages) && isNotFirstPageOrLastPage(pages) && isSizeTwo(pages);
+    }
+
+    private boolean isValidPage(List<Integer> pages) {
+        int leftPage = pages.get(0);
+        int rightPage = pages.get(1);
+        return FIRST_PAGE <= leftPage && leftPage <= LAST_PAGE
+            && FIRST_PAGE <= rightPage && rightPage <= LAST_PAGE;
+    }
+
+    private boolean isOdd(int page) {
+        return page % 2 == 1;
+    }
+
+    private boolean isEven(int page) {
+        return page % 2 == 0;
+    }
+
+    private boolean cotainsPageNumber(List<Integer> pages) {
+        return pages.get(0) != null && pages.get(1) != null;
+    }
+
+    private boolean isContinuous(List<Integer> pages) {
+        return pages.get(1) == pages.get(0) + 1;
+    }
+
+    private boolean isNotFirstPageOrLastPage(List<Integer> pages) {
+        return pages.get(0) != FIRST_PAGE && pages.get(0) != LAST_PAGE
+            && pages.get(1) != FIRST_PAGE && pages.get(1) != LAST_PAGE;
+    }
+
+    private boolean isSizeTwo(List<Integer> pages) {
+        return pages.size() == 2;
+    }
+}
