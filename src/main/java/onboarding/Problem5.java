bd11c02f4ebaa0aaa4f5c592d1447528b273eed0
prao <pch5521@gmail.com>
2023-10-16 18:05:21 +0900

    refactor: [Problem5] 화폐 단위별 변환 기능 로직 수정
    - currencies 배열 생성
    - while문을 통해 로직 단순화

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,18 +1,45 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class Problem5 {
-    public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
-    }
+class Problem5 {
 
-    static class Validation {
+	private Problem5() {
+	}
 
-        public boolean range(int money) {
-            return 1 <= money && money <= 1_000_000;
-        }
-    }
+	static Validation validation = new Validation();
+	static CurrencyExchange currencyExchange = new CurrencyExchange();
+
+	public static List<Integer> solution(int money) {
+		if (validation.range(money)) {
+			return currencyExchange.exchange(money);
+		}
+		return Collections.emptyList();
+	}
+
+	static class Validation {
+
+		public boolean range(int money) {
+			return 1 <= money && money <= 1_000_000;
+		}
+	}
+
+	static class CurrencyExchange {
+
+		public List<Integer> exchange(int money) {
+			List<Integer> currency = new ArrayList<>();
+			int[] currencies = new int[] {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+			int change = money;
+			int index = 0;
+			while (index <= 8) {
+				int count = change / currencies[index];
+				currency.add(count);
+				change %= currencies[index];
+				index++;
+			}
+			return currency;
+		}
+	}
 }
