b0b5686e1a73a3cd87fcfaf997e2758d00f63f96
prao <pch5521@gmail.com>
2023-10-16 19:01:03 +0900

    feat: [Problem6] 이메일 유효성 검사
    - checkDomain: email.com 도메인 제한
    - length: 이메일 전체 길이 11자 이상 20자 미만

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -7,4 +7,16 @@ public class Problem6 {
         List<String> answer = List.of("answer");
         return answer;
     }
+
+    static class emailValidation {
+
+        private boolean checkDomain(String email) {
+            int index = email.indexOf("@") + 1;
+            return email.substring(index).equals("email.com");
+        }
+
+        private boolean length(String email) {
+            return 11 <= email.length() && email.length() < 20;
+        }
+    }
 }
