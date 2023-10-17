31ae5ddd0412699b8d00bcb1bd7cb117ae71e36e
prao <pch5521@gmail.com>
2023-10-17 16:20:08 +0900

    feat: [Problem7] 입력값 유효성 검사 기능

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -4,8 +4,43 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem7 {
-    public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
-    }
+	public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+		List<String> answer = Collections.emptyList();
+		return answer;
+	}
+
+	static class Validation {
+
+		public boolean isValid(String user, List<List<String>> friends, List<String> visitors) {
+			return isUserLengthValid(user) && isUserLowerCase(user)
+				&& isFriendsSizeValid(friends) && isFriendSizeValid(friends)
+				&& isVisitorsSizeValid(visitors);
+		}
+
+		private boolean isUserLengthValid(String user) {
+			return 1 <= user.length() && user.length() <= 30;
+		}
+
+		private boolean isUserLowerCase(String user) {
+			return user.equals(user.toLowerCase());
+		}
+
+		private boolean isFriendsSizeValid(List<List<String>> friends) {
+			return 1 <= friends.size() && friends.size() <= 10_000;
+		}
+
+		private boolean isFriendSizeValid(List<List<String>> friends) {
+			for (List<String> friend : friends) {
+				if (friend.size() != 2) {
+					return false;
+				}
+			}
+			return true;
+		}
+
+		private boolean isVisitorsSizeValid(List<String> visitors) {
+			return 0 <= visitors.size() && visitors.size() <= 10_000;
+		}
+
+	}
 }
